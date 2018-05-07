76f8ee14325573f4359445c411a129b89230315e
Egor Frolikov <egorfrolikow@gmail.com>
2018-05-07 23:58:57 +0700

    сделал начальную версию слайдера. подвижный ползунок

--- src/slider/test.js
+++ src/slider/test.js
@@ -7,49 +7,59 @@ $(document).ready(function () {
 
 (function ($) {
   jQuery.fn.slider = function () {
-    //create slider elements
-    let createScale = $('.block_slider').eq(0).html('<div class="slider">');
-    let createSliderHandle = $('.slider').eq(0).html('<div class="slider_handle" id="test">');
-    let createSliderHandleValue = $('.slider_handle').eq(0).html('<div class="slider_handle_value">32');
-    // let sliderWidth = createSliderHandle.css('width');
-    // console.log(sliderWidth);
+    //создание элементов слайдера
+    $('.block_slider').html('<div class="slider">');
+    $('.slider').html('<div class="slider_handle" id="test">');
+    $('.slider_handle').html('<div class="slider_handle_value">');
 
-    let handle = $('.slider_handle').eq(0);
+    let sliderMin = 0;
+    let sliderMax = 100;
+    let sliderValue = 35;
 
-    handle.mousedown(function (e) {
-      // debugger;
-      var coords = getCoords(handle);
-      var shiftX = e.pageX - coords.left;
-      //shiftX = растояние от левого края страницы - левый край элемента
+    //присваивание переменных соответствующим элементам
+    let sliderElem = $('.slider:eq(0)');
+    let handleElem = $('.slider_handle:eq(0)');
+    let valueElem = $('.slider_handle_value:eq(0)');
+    //событие нажатой ЛКМ
+    handleElem.mousedown(function (event) {
+      let sliderCoords = getCoords(sliderElem); //внутренние координаты слайдера
+      let handleCoords = getCoords(handleElem); //внутренние координаты ползунка
+      let shiftX = event.pageX - handleCoords.left; //координаты левого края элемента
+      // движение нажатой ЛКМ
+      $(document).on('mousemove', function (event) {
+        //вычисление левого края слайдера и задание координат для ползунка
+        let newLeft = event.pageX - shiftX - sliderCoords.left;
+        if (newLeft < 0) {
+          newLeft = 0;
+        }
+        let rightEdge = sliderElem.outerWidth() - handleElem.outerWidth();
+        if (newLeft > rightEdge) {
+          newLeft = rightEdge;
+        }
+        handleElem.css('left', newLeft + 'px');
+      });
 
-      moveHandle(e);
-      function moveHandle(e) {
-        handle.css('left', shiftX + 'px');
-      };
+      $(document).mouseup(function () {  //событие ОТжатой ЛКМ, отмена "mousemove"
+        $(document).off('mousemove');
+      });
+      calculateSliderValue(sliderMax);
+      function calculateSliderValue(slMax) {
+        let sliderWidth = parseInt(sliderElem.css('width'));
+        let sliderStep = sliderWidth / slMax;
 
-      document.mousemove = function (e) {
-        moveHandle(e);
-      }
-
-      handle.mouseup = function () {
-        // document.mousemove = null;
-        handle.mouseup = null;
+        console.log('sliderWidth ' + sliderWidth);
+        console.log('sliderStep ' + sliderStep);
+        console.log('');
       }
     });
 
-    /////////////
-    handle.ondragstart = function () {
-      return false;
-    };
-
-    function getCoords(elem) {
-      var box = elem.get(0).getBoundingClientRect();
+    function getCoords(elem) { //получение координат курсора внутри элемента
+      let box = elem.get(0).getBoundingClientRect();
       return {
+        top: box.top + pageYOffset,
         left: box.left + pageXOffset
       };
     }
 
-    //create configs panel element
-    //создание инпутов чекбоксов и т.п.
   }
 })(jQuery);
