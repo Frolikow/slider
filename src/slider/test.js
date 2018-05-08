a9a2533275e954f333b78362529ef37c35583627
Egor Frolikov <egorfrolikow@gmail.com>
2018-05-08 13:27:03 +0700

    сделал передачу значений в ярлык над ползунком. установка ползунка на дефолтную позицию при загрузке страницы

--- src/slider/test.js
+++ src/slider/test.js
@@ -13,18 +13,23 @@ $(document).ready(function () {
     $('.slider_handle').html('<div class="slider_handle_value">');
 
     let sliderMin = 0;
-    let sliderMax = 100;
-    let sliderValue = 35;
+    let sliderMax = 10;
+    let sliderValue = 5;
 
     //присваивание переменных соответствующим элементам
     let sliderElem = $('.slider:eq(0)');
     let handleElem = $('.slider_handle:eq(0)');
     let valueElem = $('.slider_handle_value:eq(0)');
+
+    valueElem.html(sliderValue);
+    handleElem.css('left', defaultValue());
+
     //событие нажатой ЛКМ
     handleElem.mousedown(function (event) {
       let sliderCoords = getCoords(sliderElem); //внутренние координаты слайдера
       let handleCoords = getCoords(handleElem); //внутренние координаты ползунка
       let shiftX = event.pageX - handleCoords.left; //координаты левого края элемента
+
       // движение нажатой ЛКМ
       $(document).on('mousemove', function (event) {
         //вычисление левого края слайдера и задание координат для ползунка
@@ -37,21 +42,26 @@ $(document).ready(function () {
           newLeft = rightEdge;
         }
         handleElem.css('left', newLeft + 'px');
+
+        calculateSliderValue(sliderMax, sliderMin, newLeft);
       });
 
       $(document).mouseup(function () {  //событие ОТжатой ЛКМ, отмена "mousemove"
         $(document).off('mousemove');
       });
-      calculateSliderValue(sliderMax);
-      function calculateSliderValue(slMax) {
-        let sliderWidth = parseInt(sliderElem.css('width'));
-        let sliderStep = sliderWidth / slMax;
-
-        console.log('sliderWidth ' + sliderWidth);
-        console.log('sliderStep ' + sliderStep);
-        console.log('');
-      }
     });
+
+    function calculateSliderValue(slMax, slMin, leftEdge) { //расчет значения над ползунком
+      let sliderWidth = parseInt(sliderElem.css('width'));
+      let sliderStep = (sliderWidth / slMax - slMin)*0.96;
+      let currentSliderValue = leftEdge / sliderStep;
+      valueElem.html(currentSliderValue ^ 0);
+    }
+
+    function defaultValue(){  //установка ползунка в позицию "по-умолчанию" = sliderValue
+      let defaultPosition = sliderValue*((parseInt(sliderElem.css('width')) /sliderMax - sliderMin)*0.96);
+      return defaultPosition;
+    }
 
     function getCoords(elem) { //получение координат курсора внутри элемента
       let box = elem.get(0).getBoundingClientRect();
